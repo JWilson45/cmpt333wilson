caa81e3eb97b2f6a1b07c691775b38a0dc23be26
JWilson45 <jason.wilson2@marist.edu>
2020-05-01 18:19:57 -0400

    Problem 2

--- Homework/Extra Logical Predicate/P2.pl
+++ Homework/Extra Logical Predicate/P2.pl
@@ -18,13 +18,18 @@ edit(File, Command) :-
 	writeIn([Command, 'is not applicable']), !,
 	edit(File).
 
+apply(up,file([X|Xs],Ys),file(Xs,[X|Ys])).
+
+apply(up(N),file(Xs,Ys),file(Xsl,Ysl)) :-
+  N > O,
+  up(N,Xs,Ys,Xsl,Ysl).
+
+% Down
 apply(down,file([X|Xs], Ys), file(Xs, [X|Ys])).
 
 apply(down(N), file(Xs, Ys), file(Xs1, Ys1)) :-
 	N =< 0,
 	down(N, Xs, Ys, Xs1, Ys1).
-
-apply(down, file(Xs, [Y|Ys]), file([Y|Xs], Ys)).
 
 apply(insert(Line), file(Xs, Ys), file(Xs, [Line|Ys])).
 
@@ -38,6 +43,16 @@ apply(print(*), file(Xs, Ys), file(Xs, Ys)) :-
 	write_file(Xs1),
 	write_file(Ys).
 
+up(N,[ ],Ys,[ ],Ys).
+
+up(O,Xs,Ys,Xs,Ys).
+
+up(N,[X|Xs] ,Ys,Xsl,Ysi) :-
+  N > O,
+  Nl is N-1,
+  up(Nl,Xs,[XIYs],Xsl,Ysl).
+
+% Down
 down(N, [], Ys, [], Ys).
 
 down(N, Xs, Ys, Xs, Ys).
